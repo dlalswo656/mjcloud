67364586a1905f122c66fcf9b6b4124685055873
dlalswo656 <dlalswo656@gmail.com>
2026-05-06 15:31:40 +0900

    20260506 mjcloud 포트폴리오 완성

--- backend/src/main/java/com/mjcloud/MjcloudApplication.java
+++ backend/src/main/java/com/mjcloud/MjcloudApplication.java
@@ -2,7 +2,7 @@ package com.mjcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// 20260506 mjcloud 포트폴리오 완성
 @SpringBootApplication
 public class MjcloudApplication {
     public static void main(String[] args) {
